e0a1a166c389ab826ad680c1b5541e39e527027c
Nehathvarma1616 <nehathvarma@gmail.com>
2023-03-27 19:09:15 +0530

    todo-app created

---
+++ todo-app/app.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/todos", (request, response) => {
+  // response.send("hello world")
+  console.log("Todo list");
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("Creating a todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to update a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", (request, response) => {
+  console.log("Delete a todo by ID:", request.params.id);
+});
+
+module.exports = app;
---
+++ todo-app/index.js
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log("listening server at this port 3000");
+});
