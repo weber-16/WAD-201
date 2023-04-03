f9025af93c9b6831be584df01b1f45653cecac56
Nehathvarma1616 <nehathvarma@gmail.com>
2023-04-03 23:50:49 +0530

    nodemon, ejs files are created

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,10 +1,28 @@
-/*eslint-disable no-unused-vars */
-
+/* eslint-disable */
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const path = require("path");
+
+// ejs as view engine
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render('index', {
+      allTodos
+    });
+  } else {
+    response.json({
+      allTodos
+    });
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (request, response) {
   response.send("Hello World");
