f2afe92aa6382363bdfe28c58491df4aea2b29f9
Nehathvarma1616 <nehathvarma@gmail.com>
2023-04-25 20:55:24 +0530

    relationship created between Todo,User

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -8,6 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      })
       // define association here
     }
 
